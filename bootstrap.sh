b3e6056b97dc9f132534861c334b377363329504
John Dibling <johnd@spryware.com>
2014-11-26 04:08:42 -0600

    Updated bootstrapper to install YouCompleteMe

--- bootstrap.sh
+++ bootstrap.sh
@@ -2,5 +2,13 @@
 
 git submodule init
 git submodule update --recursive
+
+#
+# Install YouCompleteMe and dependant libs
+#
+echo "Installing YouCompleteMe..."
+cd bundle/YouCompleteMe
+./install.sh --clang-completer
+cd ../..
 ln -s `pwd`/3rdparty/libclang.so-RHEL6 `pwd`/bundle/YouCompleteMe/third_party/ycmd/libclang.so
 
