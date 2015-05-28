230faa48112afeb5a2d9e3b55891cc3da3138510
John Dibling <john.dibling@picotrading.com>
2015-05-28 15:23:42 -0500

    Getting rid of YCM

--- bootstrap.sh
+++ bootstrap.sh
@@ -2,25 +2,6 @@
 
 git submodule update --init --recursive
 
-#
-# Install YouCompleteMe and dependant libs
-#
-echo "Installing YouCompleteMe..."
-cd bundle/YouCompleteMe
-./install.sh --clang-completer
-cd ../..
-
-# HACK:  The version of libclang.so that ships with YouCompleteMe is dependant
-# upon a version of libedit.so that's not available on RHEL.
-# Assuming we already have libclang.so that does work,
-# let's get rid of the one that YCM gave us and symlink to ours.
-
-rm `pwd`/bundle/YouCompleteMe/third_party/ycmd/libclang.so
-ln -s `pwd`/3rdparty/libclang.so-RHEL6 `pwd`/bundle/YouCompleteMe/third_party/ycmd/libclang.so
-
-# if you have libclang available, this step is not needed
-# ln -s `pwd`/3rdparty/libclang.so-RHEL6 `pwd`/bundle/YouCompleteMe/third_party/ycmd/libclang.so
-
 # Update the user's vimrc
 if [ -f ~/.vimrc ]
 then
